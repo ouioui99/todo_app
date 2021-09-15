d4cedb0e72dd071ce9d4e1e7c81ef79304740274
ouioui25 <qp5minute@gmail.com>
2021-09-15 11:30:57 +0900

    jbuilderからのjson作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'to_dos#index'  
   resources :to_dos
 
-  namespace :api, {firmat: 'json'} do
+  namespace :api, {format: 'json'} do
     namespace :v1 do
       resources :to_dos
     end
